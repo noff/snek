8ebc3d910348ce16f9a00dc8b7eb0060ca68a104
Michael Kechinov <mkechinov@Michaels-MacBook-Pro.local>
2018-05-25 12:21:28 +0300

    More styles

--- app/services/snek_style.rb
+++ app/services/snek_style.rb
@@ -1,7 +1,7 @@
 module SnekStyle
 
   COLORS = %w[blue dark_blue dark_brown green grey light_brown lime_green mint orange pink pumpkin purple red sky_blue white yellow].freeze
-  PATTERNS = %w[1 2 3 4]
+  PATTERNS = %w[1 2 3 4 5 6 7 8]
   PATTERN_COLORS = %w[black blue dark_blue dark_brown green grey light_brown lime_green mint orange pink pumpkin purple red sky_blue yellow]
 
 end
