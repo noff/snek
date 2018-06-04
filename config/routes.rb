ed54249c6af62850411a62aa791c18410562c33e
Michael Kechinov <mkechinov@Michaels-MacBook-Pro.local>
2018-06-04 23:25:45 +0300

    REES46 event tracking

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  devise_for :users
+  devise_for :users, controllers: {
+      registrations: 'users/registrations'
+  }
 
 
   resources :sneks do
