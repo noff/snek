31a8a48849e1ee28c9513bf5c721fcd9a9a77ce5
Michael Kechinov <mkechinov@Michaels-MacBook-Pro.local>
2018-06-05 12:10:41 +0300

    Hero unit

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,7 @@
 class WelcomeController < ApplicationController
 
   def index
+    @with_hero_unit = true
     @top_sneks = Snek.all.map { |snek| {snek: snek, score: SnekScore.new(snek).score, battles: snek.snek_battles.count } }.sort { |a,b| a[:score] <=> b[:score]  }.reverse.delete_if { |s| s[:score] == 0 }.take(10)
   end
 
