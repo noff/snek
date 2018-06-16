9900783b16fb1d4f0353db06926e5b14ce3ce6b0
Michael Kechinov <mkechinov@Michaels-MacBook-Pro.local>
2018-06-16 22:07:17 +0300

    Credentials is not good

--- spec/factories/arenas.rb
+++ spec/factories/arenas.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :arena do
+    id            FFaker::Internet.rand(10000000)
     name          'Fort Square'
     area          Arena.default_arena
   end
--- spec/models/arena_spec.rb
+++ spec/models/arena_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
-#
-# RSpec.describe Arena, type: :model do
-#   let!(:arena) { create(:arena) }
-#
-#   context 'initial setup' do
-#     it 'has default arena' do
-#       expect(arena.area).to eq Arena.default_arena
-#     end
-#   end
-# end
+
+RSpec.describe Arena, type: :model do
+  let!(:arena) { create(:arena) }
+
+  context 'initial setup' do
+    it 'has default arena' do
+      expect(arena.area).to eq Arena.default_arena
+    end
+  end
+end
