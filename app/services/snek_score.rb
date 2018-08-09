887ffea1ba51e624056132e2762517e9178fba28
Michael Kechinov <mkechinov@Michaels-MacBook-Pro.local>
2018-08-09 07:48:21 +0300

    Change efficiency rating

--- app/services/snek_score.rb
+++ app/services/snek_score.rb
@@ -9,7 +9,7 @@ class SnekScore
   end
 
   def efficiency
-    @snek.snek_battles.where('created_at >= ?', 2.weeks.ago).exists? ? @snek.snek_battles.where('created_at >= ?', 2.weeks.ago).sum(:score).to_f / @snek.snek_battles.where('created_at >= ?', 2.weeks.ago).count.to_f : 0.00
+    @snek.snek_battles.where('created_at >= ?', 2.weeks.ago).count >= 10 ? @snek.snek_battles.where('created_at >= ?', 2.weeks.ago).sum(:score).to_f / @snek.snek_battles.where('created_at >= ?', 2.weeks.ago).count.to_f : 0.00
   end
 
 end
